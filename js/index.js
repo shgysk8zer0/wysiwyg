8dc2a5091084ac80aeab7aae8c990f75de0fcce9
Chris Zuber <shgysk8zer0@gmail.com>
2017-12-15 15:31:42 -0800

    Move ready event code to own script

--- js/index.js
+++ js/index.js
@@ -1,34 +1,11 @@
 import './std-js/shims.js';
 import {$} from './std-js/functions.js';
-import * as Mutations from './std-js/mutations.js';
 import deprefix from './std-js/deprefixer.js';
 import webShareApi from './std-js/webShareApi.js';
 import * as shares from './share-config.js';
-import kbdShortcuts from './std-js/kbd_shortcuts.js';
-import wysiwyg from './std-js/wysiwyg.js';
+import * as Init from './init.js';
 
 webShareApi(...Object.values(shares));
 deprefix();
 
-async function readyHandler() {
-	Mutations.init();
-	const $doc = $(document.documentElement);
-	$doc.replaceClass('no-js', 'js');
-	$('[contenteditable="true"]').keypress(kbdShortcuts);
-	if (Navigator.prototype.hasOwnProperty('share')) {
-		$('[data-share]').attr({hidden: false});
-	}
-
-	if (HTMLElement.prototype.hasOwnProperty('contextMenu')) {
-		const resp = await fetch(new URL('./wysiwyg.html', location.href));
-		const parser = new DOMParser();
-		const html = await resp.text();
-		const doc = parser.parseFromString(html, 'text/html');
-		wysiwyg(doc.body.firstElementChild);
-		const editor = document.querySelector('[contenteditable="true"]');
-		editor.setAttribute('contextmenu', doc.body.firstElementChild.id);
-		document.body.appendChild(doc.body.firstElementChild);
-	}
-}
-
-$(self).ready(readyHandler, {once: true});
+$(self).ready(Init.readyHandler, {once: true});
---
+++ js/init.js
@@ -0,0 +1,25 @@
+import {$} from './std-js/functions.js';
+import * as Mutations from './std-js/mutations.js';
+import kbdShortcuts from './std-js/kbd_shortcuts.js';
+import wysiwyg from './std-js/wysiwyg.js';
+
+export async function readyHandler() {
+	Mutations.init();
+	const $doc = $(document.documentElement);
+	$doc.replaceClass('no-js', 'js');
+	$('[contenteditable="true"]').keypress(kbdShortcuts);
+	if (Navigator.prototype.hasOwnProperty('share')) {
+		$('[data-share]').attr({hidden: false});
+	}
+
+	if (HTMLElement.prototype.hasOwnProperty('contextMenu')) {
+		const resp = await fetch(new URL('./wysiwyg.html', location.href));
+		const parser = new DOMParser();
+		const html = await resp.text();
+		const doc = parser.parseFromString(html, 'text/html');
+		wysiwyg(doc.body.firstElementChild);
+		const editor = document.querySelector('[contenteditable="true"]');
+		editor.setAttribute('contextmenu', doc.body.firstElementChild.id);
+		document.body.appendChild(doc.body.firstElementChild);
+	}
+}
