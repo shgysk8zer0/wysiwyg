474989910d095549d3523695ea71ac413ca10dc4
Chris Zuber <shgysk8zer0@gmail.com>
2018-01-04 21:22:57 -0800

    Update registering ServiceWorker

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 import './std-js/shims.js';
 import './std-js/deprefixer.js';
-import {ready} from './std-js/functions.js';
+import {ready, registerServiceWorker} from './std-js/functions.js';
 import webShareApi from './std-js/webShareApi.js';
 import * as shares from './share-config.js';
 import * as Init from './init.js';
 
-ready.then(Init.readyHandler);
+registerServiceWorker(document.documentElement.dataset.serviceWorker);
+ready().then(Init.readyHandler);
 
 webShareApi(...Object.values(shares));
